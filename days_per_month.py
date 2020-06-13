07a799d8eb93b65c3438e7333f83fba81f373817
CHENY351 <35176688+CHENY351@users.noreply.github.com>
2020-06-13 17:36:54 -0400

    Create days_per_month.py

---
+++ days_per_month.py
@@ -0,0 +1,13 @@
+# days per month
+year = int(input())
+month = int(input())
+list31 = [1, 3, 5, 7, 8, 10, 12]
+list30 = [4, 6, 9, 11]
+if month in list31:
+    print("31")
+elif month in list30:
+    print("30")
+elif month == 2 and year % 4 == 0:
+    print('29')
+else:
+    print('28')
