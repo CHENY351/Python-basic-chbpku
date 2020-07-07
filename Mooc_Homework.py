0e2ecb7b60389510e8aa79acaa12c727312b1b0b
CHENY351 <35176688+CHENY351@users.noreply.github.com>
2020-07-07 17:39:06 -0400

    Update Mooc_Homework.py

--- Mooc_Homework.py
+++ Mooc_Homework.py
@@ -1,4 +1,4 @@
-# 1. String shfit left
+# Ch3-1. String shfit left
 s = str(input())
 n = int(input())
 a = s[:n]
@@ -7,7 +7,7 @@ ss = b + a
 print(ss)
 
 
-# 2. Height of triangle
+# Ch3-2. Height of triangle
 import math
 a = int(input())
 b = int(input())
@@ -16,15 +16,21 @@ h = round(a * b / c, 2)
 print(h)
 
 
-# 3. Length of the last word in a string
+# Ch3-3. Length of the last word in a string
 s = str(input())
 n = len(s.split(' ')[-1])
 print(n)
 
 
-# 4. Times of one character appear in a string
-s = str(input())
-n = str(input())
-a = s.count(n)
-print(a)
+# Ch3-4. Times of one character appear in a string
+s = input()
+a = s.split()
+n = a[0].count(a[1])
+print(n)
 
+
+# Ch4-1. 合并两个列表并去重
+alist=list(map(int, input().split()))
+blist=list(map(int, input().split()))
+new = (list(set(alist + blist)))
+print(new)
