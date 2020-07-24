51242decb393d987ecd5baeeb53bab44f4649217
CHENY351 <35176688+CHENY351@users.noreply.github.com>
2020-07-23 20:11:22 -0400

    Update PPT_Homework.py

--- PPT_Homework.py
+++ PPT_Homework.py
@@ -81,3 +81,15 @@ print(a)
 print(a < b)
 a.moveto('Canada')
 print(a)
+
+
+# 创建一个mylist类，继承自内置数据 类型list(列表),增加一个方法“累乘”product def product(self): 返回所有数据项的乘积
+class mylist(list):
+    def product(self):
+        m = 1
+        for n in self:
+            m = n * m
+        return m
+# 调用    
+a = mylist([1,2,3])
+a.product()
